11670676fb3464505fdceb0e4c65ebfa674096e8
nicovarLI <nicolas.varela@lightit.io>
2023-08-02 11:53:02 -0300

    feat: Add properties to City model and set up fakes
    Added name to city model and migration and set up factory and seeder properties for fakes creation

--- travel-agency/database/seeders/DatabaseSeeder.php
+++ travel-agency/database/seeders/DatabaseSeeder.php
@@ -14,6 +14,7 @@ class DatabaseSeeder extends Seeder
     {
         // \App\Models\User::factory(10)->create();
 
+        \App\Models\City::factory(10)->create();
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
         //     'email' => 'test@example.com',
