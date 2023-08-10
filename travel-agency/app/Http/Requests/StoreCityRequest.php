6de8f3a93a3549698247d2f7b91190f6ebffb98b
nicovarLI <138680007+nicovarLI@users.noreply.github.com>
2023-08-10 14:29:33 -0300

    refactor: change order of min/max rules and shortened unique rule
    Co-authored-by: Gianfranco Rocco <110541583+gianfranco-rocco@users.noreply.github.com>

--- travel-agency/app/Http/Requests/StoreCityRequest.php
+++ travel-agency/app/Http/Requests/StoreCityRequest.php
@@ -22,7 +22,7 @@ class StoreCityRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'name' => ['required', 'max:255', 'min:2', 'unique:cities,name', 'regex:/^[a-zA-Z\s-]+$/']
+            'name' => ['required', 'min:2', 'max:255', 'unique:cities', 'regex:/^[a-zA-Z\s-]+$/']
         ];
     }
 }
