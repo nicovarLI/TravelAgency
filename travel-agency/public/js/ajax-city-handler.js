366f064440bd68a00a3ccbc5056247a6c77ab860
nicovarLI <nicolas.varela@lightit.io>
2023-08-10 09:53:18 -0300

    refactor: Move form data transformation into its own function

--- travel-agency/public/js/ajax-city-handler.js
+++ travel-agency/public/js/ajax-city-handler.js
@@ -8,14 +8,7 @@ $('form').submit(function(event){
 })
 function createCity(){
     var currentPage = new URLSearchParams(window.location.search).get('page') || 1;
-    var form = $('#add-city-form');
-    var formData = $(form).serializeArray();
-    var jsonData = {};
-
-    $.each(formData, function(_,field){
-        jsonData[field.name] = field.value;
-    });
-    jsonData['page']= currentPage;
+    var jsonData = formToJson($('#add-city-form'),currentPage);
 
     $.ajax({
         url: '/',
@@ -32,7 +25,7 @@ function createCity(){
             $('#cities-table').html(response.updatedCitiesTable)
             $('#pagination-links').html(response.updatedPaginationLinks);
         },
-        error: function(xhr, status, error) {
+        error: function(xhr) {
             if (xhr.status === 422) {
                 var errors = xhr.responseJSON.errors;
                 $('#name-error').text(errors.name[0]);
@@ -60,7 +53,7 @@ function deleteCity(cityId){
             $('#cities-table').html(response.updatedCitiesTable)
             $('#pagination-links').html(response.updatedPaginationLinks);
         },
-        error: function(xhr, status, error) {
+        error: function(xhr) {
             if (xhr.status === 422) {
                 var errors = xhr.responseJSON.errors;
             }
@@ -70,14 +63,7 @@ function deleteCity(cityId){
 }
 function updateCity(){
     var currentPage = new URLSearchParams(window.location.search).get('page') || 1;
-    var form = $('#cities-update-form');
-    var formData = $(form).serializeArray();
-    var jsonData = {};
-
-    $.each(formData, function(_,field){
-        jsonData[field.name] = field.value;
-    });
-    jsonData['page']= currentPage;
+    var jsonData = formToJson($('#cities-update-form'),currentPage);
 
     $.ajax({
         url: '/',
@@ -92,11 +78,21 @@ function updateCity(){
             $('#cities-table').html(response.updatedCitiesTable)
             $('#pagination-links').html(response.updatedPaginationLinks);
         },
-        error: function(xhr, status, error) {
+        error: function(xhr) {
             if (xhr.status === 422) {
                 var errors = xhr.responseJSON.errors;
             }
             console.error(xhr.responseText)
         }
     });
+}
+function formToJson(form,page){
+    var formData = $(form).serializeArray();
+    var jsonData = {};
+
+    $.each(formData, function(_,field){
+        jsonData[field.name] = field.value;
+    });
+    jsonData['page']= page;
+    return jsonData;
 }
