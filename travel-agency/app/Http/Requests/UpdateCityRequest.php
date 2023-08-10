f2d7b29820456cf76759a21fe7329f1dc20739bc
nicovarLI <nicolas.varela@lightit.io>
2023-08-10 14:35:58 -0300

    refactor: Change return type and delete useless authorize method

--- travel-agency/app/Http/Requests/UpdateCityRequest.php
+++ travel-agency/app/Http/Requests/UpdateCityRequest.php
@@ -7,17 +7,9 @@ use Illuminate\Foundation\Http\FormRequest;
 class UpdateCityRequest extends FormRequest
 {
     /**
-     * Determine if the user is authorized to make this request.
-     */
-    public function authorize(): bool
-    {
-        return true;
-    }
-
-    /**
      * Get the validation rules that apply to the request.
      *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     * @return array<string, mixed>
      */
     public function rules(): array
     {
