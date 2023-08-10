927055ec2157d5d8dde01235aed29e965bda562b
nicovarLI <138680007+nicovarLI@users.noreply.github.com>
2023-08-10 14:33:00 -0300

    refactor: Change return type of store request
    Co-authored-by: Gianfranco Rocco <110541583+gianfranco-rocco@users.noreply.github.com>

--- travel-agency/app/Http/Requests/StoreCityRequest.php
+++ travel-agency/app/Http/Requests/StoreCityRequest.php
@@ -9,7 +9,7 @@ class StoreCityRequest extends FormRequest
     /**
      * Get the validation rules that apply to the request.
      *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     * @return array<string, mixed>
      */
     public function rules(): array
     {
