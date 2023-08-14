6b7e794250f45c6dced88da4fc1cdc9648d14052
nicovarLI <nicolas.varela@lightit.io>
2023-08-14 15:58:52 -0300

    refactor: Give index route, the index name

--- routes/web.php
+++ routes/web.php
@@ -15,5 +15,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::controller(CityController::class)->group(function () {
-    Route::get('/', 'index')->name('home');
+    Route::get('/', 'index')->name('index');
 });
