901ed48edc0b4c9f5ef0832a3bc35183dff4f5c6
nicovarLI <nicolas.varela@lightit.io>
2023-08-15 09:01:48 -0300

    feat: Add working frontend link generation for table nav

--- public/js/ajax-city-handler.js
+++ public/js/ajax-city-handler.js
@@ -71,14 +71,10 @@ const loadTable = () => {
         method: 'GET',
         success:function(response)
         {
-            console.log(response);
             const cities = response.data;
             const links = response.links;
 
-            //let tableLinks = renderLinks(links);
-
             $('#table-body').html(renderTable(cities))
-            //console.log(renderTable(cities))
             $('#pagination-links').html(renderLinks(response));
         },
         error: function(xhr) {
@@ -124,6 +120,7 @@ const renderTable = (cities) => {
 }
 
 const renderLinks = (linkData) => {
+    newLinks = customizePaginationLinks(linkData.links);
     linksNav = `
     <nav role="navigation" aria-label="Pagination Navigation" class="flex items-center justify-between">
 
@@ -143,17 +140,18 @@ const renderLinks = (linkData) => {
         <div>
             <span class="relative z-0 inline-flex shadow-sm rounded-md">
 
-                <a href="${linkData.prev_page_url}" rel="prev" class="relative inline-flex items-center px-2 py-2 text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-l-md leading-5 hover:text-gray-400 focus:z-10 focus:outline-none focus:ring ring-gray-300 focus:border-blue-300 active:bg-gray-100 active:text-gray-500 transition ease-in-out duration-150" aria-label="&amp;laquo; Previous">
+                <a href="${newLinks[0].url}" rel="prev" class="relative inline-flex items-center px-2 py-2 text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-l-md leading-5 hover:text-gray-400 focus:z-10 focus:outline-none focus:ring ring-gray-300 focus:border-blue-300 active:bg-gray-100 active:text-gray-500 transition ease-in-out duration-150" aria-label="&amp;laquo; Previous">
                     <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
                         <path fill-rule="evenodd" d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clip-rule="evenodd" />
                     </svg>
                 </a>`;
-    linkData.links.forEach(link => {
+    newLinks.pop();
+    newLinks.slice(1).forEach(link => {
         linksNav += `
-        <a href="${link.url}" class="relative inline-flex items-center px-4 py-2 -ml-px text-sm font-medium text-gray-700 bg-white border border-gray-300 leading-5 hover:text-gray-500 focus:z-10 focus:outline-none focus:ring ring-gray-300 focus:border-blue-300 active:bg-gray-100 active:text-gray-700 transition ease-in-out duration-150" aria-label="Go to page ${link.label}">`;
+        <a href="${link.url}" class="relative inline-flex items-center px-4 py-2 -ml-px text-sm font-medium text-gray-700 bg-white border border-gray-300 leading-5 hover:text-gray-500 focus:z-10 focus:outline-none focus:ring ring-gray-300 focus:border-blue-300 active:bg-gray-100 active:text-gray-700 transition ease-in-out duration-150" aria-label="Go to page ${link.label}">${link.label}</a>`;
     });
     linksNav += `
-        <a href="${linkData.next_page_url}" rel="next" class="relative inline-flex items-center px-2 py-2 -ml-px text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-r-md leading-5 hover:text-gray-400 focus:z-10 focus:outline-none focus:ring ring-gray-300 focus:border-blue-300 active:bg-gray-100 active:text-gray-500 transition ease-in-out duration-150" aria-label="Next &amp;raquo;">
+        <a href="${newLinks[newLinks.length-1].url}" rel="next" class="relative inline-flex items-center px-2 py-2 -ml-px text-sm font-medium text-gray-500 bg-white border border-gray-300 rounded-r-md leading-5 hover:text-gray-400 focus:z-10 focus:outline-none focus:ring ring-gray-300 focus:border-blue-300 active:bg-gray-100 active:text-gray-500 transition ease-in-out duration-150" aria-label="Next &amp;raquo;">
         <svg class="w-5 h-5" fill="currentColor" viewBox="0 0 20 20">
             <path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd" />
         </svg>
@@ -163,3 +161,12 @@ const renderLinks = (linkData) => {
 }
 
 const currentPage = () => new URLSearchParams(window.location.search).get('page') || 1;
+
+const customizePaginationLinks = (links) => {
+    return links.map(link => {
+        if (link.url) {
+            link.url = link.url.replace('/api/cities', '');
+        }
+        return link;
+    });
+}
