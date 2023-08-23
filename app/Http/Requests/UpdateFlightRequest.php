62407f9ef6b03291a5e0444e1da58956cba0e981
nicovarLI <nicolas.varela@lightit.io>
2023-08-23 08:23:44 -0300

    feat: Create flight api controller and requests

---
+++ app/Http/Requests/UpdateFlightRequest.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class UpdateFlightRequest extends FormRequest
+{
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array|string>
+     */
+    public function rules(): array
+    {
+        return [
+            //
+        ];
+    }
+}
