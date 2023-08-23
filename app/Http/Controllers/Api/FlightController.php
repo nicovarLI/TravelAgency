eccfda007d554b8d001285670655de59077a89f8
nicovarLI <nicolas.varela@lightit.io>
2023-08-23 08:34:16 -0300

    feat: Add crud methods and index route

--- app/Http/Controllers/Api/FlightController.php
+++ app/Http/Controllers/Api/FlightController.php
@@ -3,9 +3,47 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreFlightRequest;
+use App\Http\Requests\UpdateFlightRequest;
+use App\Models\Flight;
+use Illuminate\Contracts\Pagination\LengthAwarePaginator;
+use Illuminate\Http\JsonResponse;
 use Illuminate\Http\Request;
 
 class FlightController extends Controller
 {
-    //
+    public function index(): LengthAwarePaginator
+    {
+        return Flight::paginate(10);
+    }
+
+    public function store(StoreFlightRequest $request): JsonResponse
+    {
+        Flight::create($request->validated());
+
+        return response()->json([
+            'message' => 'Flight stored.',
+            'status' => 'success',
+        ], JsonResponse::HTTP_CREATED);
+    }
+
+    public function update(UpdateFlightRequest $request, Flight $flight): JsonResponse
+    {
+        $flight->update($request->validated());
+
+        return response()->json([
+            'message' => 'Flight updated.',
+            'status' => 'success',
+        ]);
+    }
+
+    public function destroy(Flight $flight): JsonResponse
+    {
+        $flight->delete();
+
+        return response()->json([
+            'message' => 'Flight deleted.',
+            'status' => 'success',
+        ]);
+    }
 }
